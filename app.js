e09ae708d92f6c549d12a72cacfe7256bdc0d2e1
DESKTOP-L0JOP3J\edison <edisonsu0401@gmail.com>
2022-02-16 02:00:22 +0900

    added nodejs modules

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine','ejs');
 
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:false}));
 
 
 app.use('/',chatRoot);
